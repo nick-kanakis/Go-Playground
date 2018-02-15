4bf760024dc54cc9d132c73fa44ee9cb50a095b2
nicolasmanic <nicolas.kanakis@gmail.com>
2018-02-15 14:41:41 +0200

    Package visibility & unbuffered channels examples

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Go-Playground/entities"
+	"sync"
+)
 
 func main() {
 	//test how init func works
@@ -18,5 +22,31 @@ func main() {
 	//The following will fail since a value is passed and not a pointer
 	//sendNotification(admin1)
 	SendNotification(&admin1)
+
+	//testing the visibility of unexported & exported types
+	//in other packages
+	fmt.Println("#### testing package visibility")
+	//var myCar Car
+	var myCar entities.Car
+
+	//Only NumberPlate is exported from package, this does not mean that
+	//we do not have access to properties of vehicle
+	myCar = entities.Car{
+		NumberPlate: 1234,
+	}
+	//Age that is exposed value of the unexposed vehicle can still be accessed
+	//it is promoted to Car.
+	myCar.Age = 5
+	fmt.Println(myCar)
+
+	//Test unbuffered channels
+	fmt.Println("#### testing unbuffered channels")
+	var wg sync.WaitGroup
+	wg.Add(2)
+	score := make(chan int)
+	go Player("Nick", score, &wg)
+	go Player("Marg", score, &wg)
+	score <- 1
+	wg.Wait()
 
 }
